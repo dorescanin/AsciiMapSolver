73bacc0b84a5285166a9ce9f7e33726fa2e93cd2
Dario Orescanin <dorescanin@compono.hr>
2019-02-23 15:15:56 +0100

    Implement main class

--- src/main/java/hr/dorescanin/Main.java
+++ src/main/java/hr/dorescanin/Main.java
@@ -1,7 +1,28 @@
 package hr.dorescanin;
 
+import hr.dorescanin.model.AsciiMap;
+import hr.dorescanin.model.AsciiMapBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws IOException {
+        final int length = args.length;
+
+        if (length == 0) {
+            System.out.println("First argument must be filename containing map");
+            return;
+        }
+
+        final String fileName = args[0];
+
+        final byte[] bytes = Files.readAllBytes(Paths.get(fileName));
+        final String map = new String(bytes);
+
+        final AsciiMap asciiMap = AsciiMapBuilder.build(map);
+        final AsciiMapTraversal traversal = new AsciiMapTraversal(asciiMap);
+        traversal.traverse();
     }
 }
